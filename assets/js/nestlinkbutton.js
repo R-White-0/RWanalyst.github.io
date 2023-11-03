547d4188744717c86117bafddf9f209bfa3523db
RWhite <rojaywhite@icloud.com>
2023-11-03 16:43:47 -0400

    update html + js

---
+++ assets/js/nestlinkbutton.js
@@ -0,0 +1,11 @@
+function openLinks() {
+    // Define the URLs for the two links
+    var link1 = "https://github.com/R-White-0/London-Bikes/blob/master/src/Bikes.pdf";
+    var link2 = "https://public.tableau.com/views/LondonBikeSharingVisualization/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
+
+    // Open the first link in a new tab
+    window.open(link1, '_blank');
+
+    // Open the second link in another new tab
+    window.open(link2, '_blank');
+  }
